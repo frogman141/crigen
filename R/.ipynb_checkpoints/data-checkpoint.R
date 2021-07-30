9b37fac8364021f2f2818e7f6c93b8fdd6a2c2ad
Alexander Baker <baker02@clust1-headnode-1.cri.camres.org>
2021-07-30 16:15:04 +0100

    removing dyngne

--- R/.ipynb_checkpoints/data-checkpoint.R
+++
@@ -1,8 +0,0 @@
-################################################################################
-# loading gRNA Libraries On-Target and Off-Target Probability Density Functions
-################################################################################
-
-#' @name grna_lib_meta
-#' @title gRNA Library On-Target and Off-Target PDFs
-#' @format A list of lists. Each individual sub list is a grna library. for each library you have 2 dataframes containing on and off target pdfs.
-"grna_lib_meta"
